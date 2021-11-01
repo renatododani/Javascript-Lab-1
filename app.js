8a8ff4085f1890bcae732da95f045a114e96c9ed
Renato Dodani <renato.dodani1@gmail.com>
2021-10-31 20:13:15 -0400

    added all the logic for javascript

---
+++ app.js
@@ -0,0 +1,32 @@
+let name = 'Mitch Cuckovich';
+let age = 25;
+let birthday = 'January 24';
+let pineapplePizza = true;
+let lifeEvents = ['I was born in Troy, Michigan.',
+    'I went to Hope College',
+    'I went to Junior Olympics when I was 10 years old. I places 14th in the nation in the 800 meter event.',
+    'I\'m a graduate of Grand Circus bootcamp.'
+]
+
+if (pineapplePizza === true) {
+    console.log(`My name is ${name} and I like pineapples on pizza. I am currently ${age} years old and my birthday is on ${birthday}.`);
+} else {
+    console.log(`My name is ${name} and I\'m not into pineapples on pizza. I am currently ${age} years old and my birthday is on ${birthday}.`)
+}
+
+for (let i = 0; i < lifeEvents.length; i++) {
+    console.log(lifeEvents[i]);
+}
+
+let counter = 0;
+while (true) {
+    let randomNumber = Math.floor(Math.random() * 11);
+    if (randomNumber !== 5) {
+        counter++;
+        console.log(`${randomNumber} !== 5`);
+    } else {
+        counter++;
+        console.log(`5 === 5. It took ${counter} iterations to randomly generate the number 5`);
+        break;
+    }
+}
